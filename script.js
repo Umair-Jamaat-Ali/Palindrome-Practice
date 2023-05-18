592bd92a79f809792fa5781a1c364efae92afd29
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-05-18 23:23:58 +0500

    practice

---
+++ script.js
@@ -0,0 +1,24 @@
+//// Palindrome Checker: Create a program that takes a string input from the user
+/// and checks if it is a palindrome (i.e., the string reads the same forwards and backwards).
+//// The program should then display the result on the webpage.
+/// Palindrome mein hum un string ko check krty hain jo word same trah prhy jaty hain... ex madam, 
+
+function onPalindrome() {
+    const inputString = document.getElementById("string");
+   const string = inputString.value.toLowerCase();
+
+  const reverseString = string.split(" ").reverse().join(" ");
+
+   let result;
+
+    if(string === reverseString){
+        result = "This is Palindrome";
+    }
+    else{
+        result = "This is not Palindrome";
+    }
+
+   const resultString = document.getElementById("result");
+     resultString.textContent = result;
+
+}
